709bb5a3f394c945e2fe50894b111ecac5204302
paulgreg <paulgreg@gmail.com>
2012-09-29 22:01:51 +0200

    Fix count number

--- src/js/thermomix-views.js
+++ src/js/thermomix-views.js
@@ -2,7 +2,12 @@
     "use strict";
 
     App.categories.render = function(placeholder) {
-        var withCount = _.map(App.data.categories, function(e) { e.count = 3; return e; });
+        var withCount = _.map(App.data.categories, function(c) { 
+            c.count = _.filter(App.data.recipes, function(r) {
+                return r.categoryId === c.id;
+            }).length;
+            return c; 
+        });
         var categories = App.categories.tpl({ 'categories': withCount });
         $(placeholder).html(categories);
     }
