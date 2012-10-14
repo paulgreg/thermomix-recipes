94a9a1b95385e1e4fd20fd9d1a15b49bae140b33
paulgreg <paulgreg@gmail.com>
2012-10-14 17:13:57 +0200

    Handle network disconnection

--- js/thermomix-main.js
+++ js/thermomix-main.js
@@ -6,17 +6,38 @@
     App.recipes = {};
     App.recipe = {};
 
+    { // Markdown converter
+        App.converter = Markdown.getSanitizingConverter();
 
-    App.converter = Markdown.getSanitizingConverter(); 
+        var baseUrl = window.location.protocol + '//' + window.location.host + window.location.pathname;
 
-    var baseUrl = window.location.protocol + '//' + window.location.host + window.location.pathname;
-
-    App.converter.hooks.chain("preConversion", function (text) {
-        var t = text.replace(/!\[([^\]]*)\]\(([^\)]*)\)/g, function(matcher, desc, img) {
-            return '!['+desc+']('+baseUrl+img+')';
+        App.converter.hooks.chain("preConversion", function (text) {
+            var t = text.replace(/!\[([^\]]*)\]\(([^\)]*)\)/g, function(matcher, desc, img) {
+                return '!['+desc+']('+baseUrl+img+')';
+            });
+            return t;
         });
-        return t;
-    });
+    }
 
+    { // Block edit/save/delete when offline
+        $(function() {
+            var $connectionStatus = $('#connection-status');
+            var setReadOnly = function() {
+                $connectionStatus.find('.online').hide();
+                $connectionStatus.find('.offline').show();
+                $('a.action').each(function(i, a) {
+                    $(a).hide();
+                });
+                alert('La connection semble avoir été perdue. Seule la consultation est possible.');
+            };
+
+            window.addEventListener("offline", setReadOnly, false);
+            window.addEventListener("online", function() {
+                alert('La connection semble avoir été retrouvé, veuillez rafraîchir la page pour pouvoir à nouveau ajouter, modifier ou supprimer des recettes ou catégories.');
+            }, false);
+
+            if (!navigator.onLine) setReadOnly();
+        });
+    }
 
 }(window.App = window.App || {}, jQuery));
