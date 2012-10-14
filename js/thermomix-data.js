a1af635e4dbd77346e757dbde48ee7f596d87081
paulgreg <paulgreg@gmail.com>
2012-10-14 18:11:11 +0200

    Simple check to avoid data override

--- js/thermomix-data.js
+++ js/thermomix-data.js
@@ -1,33 +1,23 @@
 (function (App, $, undefined) {
     "use strict";
     
-    App.data = window.localStorage.data || {};
     App.configuration = {
         'datastoreUrl': 'datastore.php?n=',
         'cookbook': window.localStorage.cookbook || 'default'
     };
+    App.data = window.localStorage.data || {};
 
-    App.loadData = function() {
+    var load = function(onSuccess, onError) {
         $.ajax({
             url: App.configuration.datastoreUrl + App.configuration.cookbook, 
             dataType: 'json',
             async: false, // Do not use Ajax since data are mandatory
-            success: function onSuccess(data) {
-                App.data = data;
-                window.localStorage.data = JSON.stringify(App.data); // refresh localStorage
-            },
-            error: function onError(xhr) {
-                console.error('App.loadData', xhr);
-                if (xhr.status === 404) {
-                    // Cookbook doesn’t exist, try to create one 
-                    App.data = {categories: [{id:1,name:"default"}], recipes: []};
-                    App.saveData();
-                }
-            }
+            success: onSuccess,
+            error: onError
         });
     };
 
-    App.saveData = function() {
+    var save = function() {
         $.ajax({
             type: 'POST',
             url: App.configuration.datastoreUrl + App.configuration.cookbook, 
@@ -39,9 +29,42 @@
                 window.localStorage.data = App.data; // refresh localStorage
             },
             error: function onError(data) {
-                console.error('App.saveData', data);
-                alert('failed to save data on server');
+                console.error('App.saveData on save', data);
+                alert('Erreur lors de la sauvegarde');
             }
+        });
+    };
+
+    App.loadData = function() {
+        load(function onSuccess(data) {
+            App.data = data;
+            window.localStorage.data = JSON.stringify(App.data); // refresh localStorage
+        }, function onError(xhr) {
+            console.error('App.loadData', xhr);
+            if (xhr.status === 404) {
+                // Cookbook doesn’t exist, try to create one 
+                App.data = {categories: [{id:1,name:"default"}], recipes: [], lastSave: (new Date()).getTime() };
+                save();
+            }
+        });
+    };
+
+    App.saveData = function() {
+        load(function onSuccess(dataOnServer) {
+            var shouldSave = undefined;
+            // Basic timestamp check to avoid overiding data
+            if (dataOnServer.lastSave === undefined || parseInt(dataOnServer.lastSave, 10) === App.data.lastSave) { 
+                shouldSave = true;
+            } else {
+                shouldSave = confirm('Les donnés sur le serveur semblent plus récentes que celles chargées dans l’application. Vous pouvez forcer la sauvegarde mais cela risque d’écraser des données sur le serveur. Dans le doute, cliquez sur annuler et rafraîchissez la page.');
+            }
+            if (shouldSave === true) {
+                App.data.lastSave = (new Date()).getTime();
+                save();
+            }
+        }, function onError() {
+            console.error('App.saveData on load to check timestamp', xhr);
+            alert('Erreur lors de la sauvegarde');
         });
     };
 
@@ -49,7 +72,7 @@
         App.configuration.cookbook = newCookbook;
         window.localStorage.cookbook = newCookbook;
         App.loadData();
-    }
+    };
 
     // Load data at startup if browser is connected
     if (navigator.onLine && window.ThermomixRecipesUnderTest !== true) App.loadData();
