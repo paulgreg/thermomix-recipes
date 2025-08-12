863c73f2bbba9fef9aa4a4d75e4b9919786da5e3
Grégory Paul <me@gpaul.me>
2025-08-12 18:57:25 +0200

    chore: move base url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
+import settings from './src/settings.json'
 
 export default defineConfig(() => {
     return {
         build: {
-            sourcemap: true
+            sourcemap: true,
         },
-        base: './',
+        base: settings.baseUrl,
         plugins: [react(), svgrPlugin()],
         test: {
             include: ['**/*.test.js'],
